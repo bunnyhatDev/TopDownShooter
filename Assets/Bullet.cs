f93495f5a88694cf772f4186e2a09d6de943c647
Zee M <zee_shan265@hotmail.com>
2020-04-07 00:09:50 -0400

    added HP to enemies via Text Mesh, and a deadzone to prevent turrets from shooting under them

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -40,7 +40,7 @@ public class Bullet : MonoBehaviour {
         return;
     }
 
-    void OnCollisionEnter2D(Collision2D other) {
+    void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Enemy") {
             other.gameObject.GetComponent<Enemy>().health -= (int)bulletDamage;
             Destroy(gameObject);
--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
     public EnemyTypes enemyType;
     public int health;
+    public TextMesh healthText;
     public float moveSpeed;
 
     Rigidbody2D m_rb;
@@ -16,11 +17,18 @@ public class Enemy : MonoBehaviour {
     }
 
     private void Update() {
-        if(health <= 0) {
+        healthText.text = health.ToString();
+        if (health <= 0) {
             health = 0;
             Destroy(gameObject);
         } else {
             m_rb.velocity = new Vector2(0f, -moveSpeed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.name == "Deadzone") {
+            Destroy(this.gameObject);
         }
     }
 }
